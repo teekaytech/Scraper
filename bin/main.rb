0d2f6557e107aba09e9fe55b303b8332706be886
teekaytech <taofeek.olalere@feghas.com>
2020-05-19 22:01:02 +0100

    Add scrape method to fetch urls of each category

--- bin/main.rb
+++ bin/main.rb
@@ -1,44 +1,8 @@
 require './lib/scrape.rb'
-require 'nokogiri'
-require 'httparty'
 
 # Fetch and parse HTML document
-url = 'https://javascript.info/'
-unparsed_page = HTTParty.get(url)
-parsed_page = Nokogiri::HTML(unparsed_page)
-
-#we have 3 sections in general
-sections = parsed_page.css('section.frontpage-content__container') # 3
-# getting the title of each part
-parts = Array.new
-sections.each do |part| 
-  item = part.css('h2.frontpage-content__title').text
-  parts << item
-end
-
-# number of categories == 27
-categories = parsed_page.css('div.list')
-# for each of the categories, we print the sub -  categories 
-cats = Array.new
-categories.each do |cat|
-  m = cat.css('div.list__item')
-  m.each do |a|
-    cats << a.css('div.list__title')
-  end
-end
-
-# cats.each { |a| puts a.text }
-
-category_url = Array.new
-categories.each do |cat|
-  m = cat.css('div.list__item')
-  m.each do |a|
-    category_url << a.css('div.list__title a').map { |link| link['href'] }
-  end
-end
-
-# puts category_url
+url = 'https://javascript.info'
 
 mn = Scrape.new(url)
 
-puts mn.parts
+puts mn.categories_with_url
--- lib/scrape.rb
+++ lib/scrape.rb
@@ -23,11 +23,50 @@ class Scrape < ScrapeInstruction
   end
 
   def parts
-    all_parts = complete_page.css('section.frontpage-content__container')
     parts_arr = Array.new
-    all_parts.each { |part| parts_arr << part.css('h2.frontpage-content__title').text }
+    fetch_parts.each { |part| parts_arr << part.css('h2.frontpage-content__title').text }
     parts_arr
   end
-  
+
+  def categories_title
+    cat_title = []
+    categories.each { |category| cat_title << category.text }
+    cat_title
+  end
+
+  def categories_with_url
+    cat_with_url = []
+    categories.each do |category| 
+      sub_url = category.css('div.list__title a').map { |link| link['href'] }
+      cat = {
+        title: category.text,
+        url: @url+sub_url[0]
+      }
+      cat_with_url << cat
+    end
+    cat_with_url
+  end
+
+  def categories
+    categories = Array.new
+    fetch_categories.each do |cat|
+      cat_node = cat.css('div.list__item')
+      cat_node.each do |node|
+        categories << node.css('div.list__title')
+      end
+    end
+    categories
+  end
+
+  private 
+
+  def fetch_categories
+    complete_page.css('div.list')
+  end
+
+  def fetch_parts
+    complete_page.css('section.frontpage-content__container')
+  end
+
 end
 
