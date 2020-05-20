9bb790322e20dabc466f4158a67fa6092d0518d9
teekaytech <taofeek.olalere@feghas.com>
2020-05-20 10:08:41 +0100

    Create scrape_spec_rb file for test cases

---
+++ spec/scrape_spec.rb
@@ -0,0 +1,28 @@
+require './lib/scrape'
+
+describe Scrape do
+  # let(:player) { Player.new('player1') }
+  # let(:player2) { Player.new('player2', 2) }
+
+  # describe '#icon' do
+  #   it 'returns the icon that represents player1' do
+  #     expect(player.icon).to eql('X')
+  #   end
+
+  #   it 'returns the icon that represents a player2' do
+  #     expect(player2.icon).to eql('O')
+  #   end
+  # end
+
+  # describe '#win_message' do
+  #   it 'returns the winning message for the player that wins the game' do
+  #     expect(player.win_message).to eql('Congratulations!!! player1 is the Winner...')
+  #   end
+  # end
+
+  # describe '#false_move_message' do
+  #   it 'returns a message for any false move by a player' do
+  #     expect(player.false_move_message).to eql('Invalid move by player1, try again!')
+  #   end
+  # end
+# end
