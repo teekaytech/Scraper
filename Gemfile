a6838d554102c07ef89c389e8193965142ac242e
teekaytech <taofeek.olalere@feghas.com>
2020-05-20 12:27:02 +0100

    Fix linter issues in gemfile and main.rb file

--- Gemfile
+++ Gemfile
@@ -3,5 +3,5 @@ source 'https://rubygems.org'
 git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
 # gem "rails"
-gem 'nokogiri', '~> 1.10', '>= 1.10.9'
 gem 'httparty', '~> 0.13.7'
+gem 'nokogiri', '~> 1.10', '>= 1.10.9'
--- bin/main.rb
+++ bin/main.rb
@@ -12,15 +12,15 @@ case option.to_i
 when 1
   sections = new_scrape.parts
   puts "There are #{sections.count} basic Sections, namely: "
-  sections.each_with_index { |part, i| puts "#{i + 1}. #{part}"}
+  sections.each_with_index { |part, i| puts "#{i + 1}. #{part}" }
 when 2
   cats = new_scrape.categories_title
   puts "The tutorial is divided into #{cats.count} Categories:"
-  cats.each_with_index { |cat, i| puts "#{i + 1}. #{cat}"}
+  cats.each_with_index { |cat, i| puts "#{i + 1}. #{cat}" }
 when 3
   puts "All Categories with URL: \n"
   new_scrape.categories_with_url.each do |cat_url|
-    cat_url.each { |cat, url| puts "#{cat} #{url} " }
+    cat_url.each { |cat, nurl| puts "#{cat} #{nurl} " }
     puts ''
   end
 else
